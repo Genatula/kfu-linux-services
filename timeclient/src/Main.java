00f8bbfb06b7e10528e8dc72917f9a22d446a224
Ilnas <genatula@protonmail.com>
2022-09-29 17:31:39 +0300

    Added interactive user input

--- timeclient/src/Main.java
+++ timeclient/src/Main.java
@@ -1,11 +1,15 @@
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Starting the client . . .");
         Timeclient timeclient = new TimeclientImpl();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an IP-address: ");
+        String userInput = scanner.next();
         try {
-            timeclient.connect("127.0.0.1");
+            timeclient.connect(userInput);
         } catch (UnknownHostException e) {
             System.out.println("Invalid IP-address");
         }
